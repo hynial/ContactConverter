826c9842689013fbf084c8441cbb94bc7cefd86d
hynial.chen <hynials@gmail.com>
2021-01-29 21:04:52 +0800

    fix load properties chinese encode error

--- src/main/java/com/hynial/util/PropertyUtil.java
+++ src/main/java/com/hynial/util/PropertyUtil.java
@@ -2,14 +2,17 @@ package com.hynial.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 public class PropertyUtil {
     public static Properties getProperties(){
         Properties properties = new Properties();
-        InputStream inputStream = PropertyUtil.class.getClassLoader().getResourceAsStream("ContactConverter.properties");
+//        InputStream inputStream = PropertyUtil.class.getClassLoader().getResourceAsStream("ContactConverter.properties");
         try {
-            properties.load(inputStream);
+//            properties.load(inputStream);
+            // use InputStreamReader to fix chinese encode error.
+            properties.load(new InputStreamReader(PropertyUtil.class.getClassLoader().getResourceAsStream("ContactConverter.properties"), "UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
         }
