af5e4a02ede48148a090ba7674392a803458a534
hynial.chen <hynials@gmail.com>
2021-01-29 21:05:36 +0800

    reform data

--- src/main/java/com/hynial/biz/VcfReader.java
+++ src/main/java/com/hynial/biz/VcfReader.java
@@ -46,13 +46,59 @@ public class VcfReader extends AbstractReader<ContactsInfo> {
                     itemRecord += line + "^^^";
                 }
             }
-
+            // data reform
+            reform(contactsInfoList);
             return contactsInfoList;
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
             return null;
         }
+    }
+
+    private void reform(List<ContactsInfo> contactsInfoList){
+        if(contactsInfoList == null) return ;
+        for(ContactsInfo contactsInfo : contactsInfoList){
+            String lastName = contactsInfo.getLastName();
+            String firstName = contactsInfo.getFirstName();
+            if(CommonUtil.isEmpty(lastName) && CommonUtil.isNotEmpty(firstName)){
+                // reset first name & last name
+                if(!CommonUtil.containsHanScript(firstName)) continue;
+
+                if(firstName.length() > 1) {
+                    String preTwoChar = firstName.substring(0, 2).trim();
+                    if(BizUtil.getMultiFamilyNames().contains(preTwoChar)){
+                        lastName = preTwoChar;
+                        firstName = firstName.substring(2);
+                    }else if(!BizUtil.notFamilyNames().contains(firstName.substring(0,1))){
+                        lastName = firstName.substring(0,1);
+                        firstName = firstName.substring(1);
+                    }
+                }
+            }
+
+//            firstName = firstName.replaceAll("长号|昆明|泉州|深圳", "");
+//            firstName = removeToNote(firstName, contactsInfo);
+//            firstName = firstName.replaceAll("~服装设计|new|电信|胡大侠", "");
+            contactsInfo.setFirstName(firstName);
+            contactsInfo.setLastName(lastName);
+        }
+    }
+
+    private String removeToNote(String firstName, ContactsInfo contactsInfo){
+        String[] arr = new String[]{"老师", "体系结构", "宿舍长", "律师", "TC", "科目三", "安溪", "JesseLiu", "~同门"
+                , "博士", "机械", "乒乓", "-杭电", "义工", "驾照", "353", "浙", "驾校同学", "：读经班", "心理学", "开三", "湖北黄石"};
+        arr = null;
+        if(arr == null) return firstName;
+
+        for(String rms : arr) {
+            if (firstName.endsWith(rms)) {
+                contactsInfo.setNotes((contactsInfo.getNotes() == null ? "" : contactsInfo.getNotes()) + " " + rms);
+            }
+            firstName = firstName.replaceAll(rms, "");
+        }
+
+        return firstName;
     }
 
     private void matchFields(ContactsInfo contactsInfo, String recordParam) {
@@ -72,9 +118,14 @@ public class VcfReader extends AbstractReader<ContactsInfo> {
                             Class<?> parameterizedTypeActualTypeArgument = (Class<?>) parameterizedType.getActualTypeArguments()[0];
                             if (parameterizedTypeActualTypeArgument.isAssignableFrom(String.class)) {
                                 // String
+                                boolean isMobile = "Mobile Phone".equals(aliasField.value());
                                 List<String> list = new ArrayList<>();
                                 while (matcher.find()) {
-                                    list.add(matcher.group(1));
+                                    String matchVal = matcher.group(1);
+                                    if(isMobile && matchVal != null){
+                                        matchVal = matchVal.replaceAll("[ \\-]|\\+86", "");
+                                    }
+                                    list.add(matchVal);
                                 }
                                 f.set(contactsInfo, list);
                             } else if (parameterizedTypeActualTypeArgument.isAssignableFrom(AddressInfo.class)) {
